4d09def8482ab04c5094e47ed4e7e1dc20d453fc
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-28 13:58:38 -0400

    working fine locally

--- server/app.js
+++ server/app.js
@@ -14,9 +14,9 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/api/hollers", async (req, res) => {
-  // TODO: Get a list of messages sent from a specific number
+  
   const sentMessages = await client.messages.list({$from: twilioPhoneNumber});
-  // TODO: Gather only the body of those messages for sending to the client
+
   const hollers = sentMessages.map(message => message.body);
   res.json(hollers);
 });
@@ -24,8 +24,8 @@ app.get("/api/hollers", async (req, res) => {
 app.post("/api/hollers", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
-  const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.holler}. See more Hollers at ${req.headers.referer}`;
-  // TODO: Send a message
+  const body = `${req.body.sender} says: ${req.body.receiver} , ${req.body.holler}.`;
+  await client.messages.create({to, from, body});
   res.json({ success: false });
 });
 
