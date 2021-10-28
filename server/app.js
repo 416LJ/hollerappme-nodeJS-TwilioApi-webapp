c7da1a5d3ab9a40d91fc97c919b2923546824774
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-28 15:02:16 -0400

    working fine locally again

--- server/app.js
+++ server/app.js
@@ -5,10 +5,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000 || 80;
 const twilioClient = require("twilio");
-const client = new twilioClient(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+const client = new twilioClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 app.use(cors());
 // This is a single page application and it's all rendered in public/index.html
@@ -17,9 +14,10 @@ app.use(express.static("public"));
 app.use(bodyParser.json());
 
 app.get("/api/hollers", async (req, res) => {
-  const sentMessages = await client.messages.list({ $from: twilioPhoneNumber });
+  
+  const sentMessages = await client.messages.list({$from: twilioPhoneNumber});
 
-  const hollers = sentMessages.map((message) => message.body);
+  const hollers = sentMessages.map(message => message.body);
   res.json(hollers);
 });
 
@@ -27,13 +25,8 @@ app.post("/api/hollers", async (req, res) => {
   const to = req.body.to;
   const from = process.env.TWILIO_PHONE_NUMBER;
   const body = `${req.body.sender} says: ${req.body.receiver} , ${req.body.holler}.`;
-  try {
-    await client.messages.create({ to, from, body });
-  } catch (err) {
-    res.status(err.status).json({success: false, message: err.message});
-  }
+  await client.messages.create({to, from, body});
+  res.json({ success: false });
 });
 
-app.listen(port, () =>
-  console.log(`Holler @PP Server is listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`Holler @PP Server is listening on port ${port}!`));
