4e608b0a9940f8e2066b82b24f3726f9eb2ab438
Laxsan Jeya <l.jeya86@gmail.com>
2021-10-28 01:05:44 -0400

    fixed issues locally holler-@app

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000 || 80;
 const twilioClient = require("twilio");
 const client = new twilioClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
-
+app.use(cors());
 // This is a single page application and it's all rendered in public/index.html
 app.use(express.static("public"));
 // Parse the body of requests automatically
